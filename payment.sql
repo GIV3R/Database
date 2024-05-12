f7af0650c08ec1e6e72991a90f4172eca5014d93
Rohit C <124383636+RO21T@users.noreply.github.com>
2024-05-12 10:34:40 +0530

    Create payment.sql

---
+++ payment.sql
@@ -0,0 +1,12 @@
+-- Create table payment
+CREATE TABLE payment (
+    id SERIAL PRIMARY KEY,
+    amount DOUBLE PRECISION NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    campaign_id BIGINT NOT NULL,
+    to_name VARCHAR(255) NOT NULL,
+    from_name VARCHAR(255) NOT NULL,
+    ngo_id INTEGER NOT NULL,
+    to_id INTEGER NOT NULL,
+    from_id INTEGER NOT NULL
+);
